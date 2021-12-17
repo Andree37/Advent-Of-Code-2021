80bae963f7a48506342f324235d32e650f11352a
Andre <andresmribeiro@gmail.com>
2021-12-17 16:37:43 +0000

    day 2 solution

--- common.go
+++ common.go
@@ -5,7 +5,43 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 )
+
+type NumberStringInput struct {
+	Key   string
+	Value int
+}
+
+func ReadNumberStringInput(input string) []NumberStringInput {
+	var arr []NumberStringInput
+	f, err := os.Open(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(f)
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		keyvalue := strings.Split(scanner.Text(), " ")
+		key := keyvalue[0]
+		value, err := strconv.Atoi(keyvalue[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		line := NumberStringInput{
+			Key: key, Value: value,
+		}
+		arr = append(arr, line)
+	}
+	return arr
+}
 
 func ReadNumberInput(input string) []int {
 	var arr []int
@@ -13,7 +49,6 @@ func ReadNumberInput(input string) []int {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	defer func(f *os.File) {
 		err := f.Close()
 		if err != nil {
