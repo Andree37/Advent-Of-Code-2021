1b99a558ad6b43fdf0825ce270d00f8635b26d14
Andre <andresmribeiro@gmail.com>
2021-12-19 00:21:29 +0000

    day 4 part 1 and 2 solution

--- common.go
+++ common.go
@@ -13,6 +13,16 @@ type NumberStringInput struct {
 	Value int
 }
 
+type Cell struct {
+	Value int
+	Found bool
+}
+
+type Board struct {
+	Rows  [][]Cell
+	Found bool
+}
+
 func ReadStringInput(input string) []string {
 	var arr []string
 	f, err := os.Open(input)
@@ -85,4 +95,65 @@ func ReadNumberInput(input string) []int {
 		arr = append(arr, intVar)
 	}
 	return arr
+}
+
+func ReadNumbersAndBoard(input string) ([]int, []Board) {
+	var inputs []int
+	var boards []Board
+
+	f, err := os.Open(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(f)
+
+	scanner := bufio.NewScanner(f)
+
+	board := Board{Found: false}
+	var currentRow []Cell
+
+	first := true
+	for scanner.Scan() {
+		line := scanner.Text()
+		if first {
+			var splitInput = strings.Split(line, ",")
+			for _, s := range splitInput {
+				intVar, err := strconv.Atoi(s)
+				if err != nil {
+					log.Fatal(err)
+				}
+				inputs = append(inputs, intVar)
+			}
+		} else {
+			splitLine := strings.Split(line, " ")
+			for _, l := range splitLine {
+				if l != "" {
+					intVar, err := strconv.Atoi(l)
+					if err != nil {
+						log.Fatal(err)
+					}
+					currentRow = append(currentRow, Cell{Found: false, Value: intVar})
+				}
+			}
+
+			if len(currentRow) == 5 {
+				board.Rows = append(board.Rows, currentRow)
+				currentRow = []Cell{}
+			}
+
+			if len(board.Rows) == 5 {
+				boards = append(boards, board)
+				board = Board{}
+			}
+		}
+
+		first = false
+	}
+
+	return inputs, boards
 }
